4a67224700ce35aa22d8e5cfa394669b20fcd887
Geoffrey M. Sametz <sametz@udel.edu>
2017-03-25 09:46:03 -0400

    A minimal set of tests in test_plot tests dnmrplot_2spin's replication of the default system used in WINDNMR. testdata.py created to store datasets (TODO: use another data type such as csv and read from it instead). Once minted, these initial data sets should become an INVARIANT component of the program.  TODO: add tests for dnmrmath and for main.

--- pydnmr/test_dnmrmath.py
+++ pydnmr/test_dnmrmath.py
@@ -1,6 +1,11 @@
 # TODO: Write tests!
+import numpy as np
+
 # from dnmrmath import *
 
+
+
+#def test_twospin
 
 def test_derp():
     assert 1 + 1 == 3
--- pydnmr/test_plot.py
+++ pydnmr/test_plot.py
@@ -1,21 +1,37 @@
-# TODO: create meaningful tests that pass
-
+import numpy as np
+from . import testdata
 from .dnmrplot import dnmrplot_2spin
 
 
-def test_dnmrplot_2spin_type():
+def test_dnmrplot_2spin_slowexchange():
 
-    WINDNMR_DEFAULT = (165.00, 135.00, 1.50, 0.50, 0.50, 0.5000)
+    WINDNMR_DEFAULT = (165.00, 135.00, 1.50, 0.50, 0.50, 50.00)
     x, y = dnmrplot_2spin(*WINDNMR_DEFAULT)
-    print('x ', type(x), "y", type(y))
-    assert type(x) == "<class 'numpy.ndarray'>"
-    assert type(y) == "<class 'numpy.ndarray'>"
+    accepted_x, accepted_y = testdata.TWOSPIN__SLOW
+    np.testing.assert_array_almost_equal(x, accepted_x)
+    np.testing.assert_array_almost_equal(y, accepted_y)
 
 
-if __name__ == "__main__":
-    import plottools as pt
+def test_dnmrplot_2spin_coalesce():
 
-    WINDNMR_DEFAULTS = (165.00, 135.00, 1.50, 0.50, 0.50, 50.00)
-    spectrum = dnmrplot_2spin(*WINDNMR_DEFAULTS)
-    pt.popplot(*spectrum)
+    WINDNMR_DEFAULT = (165.00, 135.00, 65.9, 0.50, 0.50, 50.00)
+    x, y = dnmrplot_2spin(*WINDNMR_DEFAULT)
+    accepted_x, accepted_y = testdata.TWOSPIN_COALESCE
+    np.testing.assert_array_almost_equal(x, accepted_x)
+    np.testing.assert_array_almost_equal(y, accepted_y)
+
+
+def test_dnmrplot_2spin_fastexchange():
+
+    WINDNMR_DEFAULT = (165.00, 135.00, 1000.00, 0.50, 0.50, 50.00)
+    x, y = dnmrplot_2spin(*WINDNMR_DEFAULT)
+    accepted_x, accepted_y = testdata.TWOSPIN_FAST
+    np.testing.assert_array_almost_equal(x, accepted_x)
+    np.testing.assert_array_almost_equal(y, accepted_y)
+# if __name__ == "__main__":
+#     import plottools as pt
+#
+#     WINDNMR_DEFAULTS = (165.00, 135.00, 1.50, 0.50, 0.50, 50.00)
+#     spectrum = dnmrplot_2spin(*WINDNMR_DEFAULTS)
+#     pt.popplot(*spectrum)
 
