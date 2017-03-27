ac4ed372ff24be72c4fccf7324225ef4b7e64507
Geoffrey M. Sametz <sametz@udel.edu>
2017-03-27 12:34:17 -0400

    For safety, branching off tests to construct a possible question for SO.

--- test_main.py
+++ test_main.py
@@ -35,12 +35,46 @@ class TestMainGUi:
     def test_two_singlet_grid_layout(self):
         """The user sees that the top of the window is a 2 x 5 grid of 
         numerical entries (bottom row) with corresponding labels (top row)"""
+        va_box_fetch = self.ui.findChild(QDoubleSpinBox, 'va')
+        print(va_box_fetch.parent().parent().objectName())
+        found_widget = self.ui.centralWidget().layout().itemAtPosition(0, 0)
+        print(found_widget.widget().text())
+        assert found_widget.widget().text() == 'Va'
+        #assert found_widget.objectName == 'va'
+        #assert va_box_fetch.objectName() == 'va'
 
-        assert 1 == 2  # TODO: Write more tests!
+        #assert 1 == 2  # TODO: Write more tests!
         # # print(self.ui.layout().itemAt(0).text)
         # va_box_fetch = self.ui.findChild(QDoubleSpinBox, 'va')
         # assert va_box_fetch.objectName() == 'va'
         # # assert self.ui.layout().itemAt(0) is not None
+
+    def test_alternate_widget_acces(self):
+        """Just hacking away trying to find a more terse widget reference"""
+        found_widget = self.ui.centralWidget().layout().itemAtPosition(0, 0)
+        print('Widget at coorinate 0, 0 is', type(found_widget))
+        print('This widget contains widget ', type(found_widget.widget()))
+        print(found_widget.widget().text())
+        assert found_widget.widget().text() == 'Va'
+        found_widget2 = self.ui.centralWidget().layout().itemAtPosition(0,0)\
+            .widget()
+        print(type(found_widget2), found_widget2.objectName(),
+              found_widget2.text())
+
+    def test_child_parent_map(self):
+        va_widget_fetch = self.ui.findChild(QDoubleSpinBox, 'va')
+        current_widget = va_widget_fetch
+        end = False
+        while not end:
+            try:
+                print('Widget ', current_widget.objectName(), ' has parent ',
+                      current_widget.parent().objectName())
+                current_widget = current_widget.parent()
+            except:
+                print('The parent of', current_widget.objectName(),
+                      'is of type', type(current_widget.parent()))
+                print('No more parents.')
+                end = True
 
     def teardown(self):
         pass
